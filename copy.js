3df602bd5adb89fd8e18d6696abe454fdde10708
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-01-07 02:56:27 +0100

    Clear game folder before copying

--- copy.js
+++ copy.js
@@ -1,11 +1,17 @@
-const fs = require("fs-extra")
+const { copySync, readdirSync, removeSync } = require("fs-extra")
 const notifier = require("node-notifier")
 
 const paths = {
   game: "D:/Spiele/SteamLibrary/steamapps/common/7 Days To Die",
 }
+paths.mods = paths.game + "/Mods"
 
-fs.copySync("dist", paths.game + "/Mods")
+readdirSync(paths.mods)
+  .filter(name => name.startsWith("n4bb12_"))
+  .map(name => paths.mods + "/" + name)
+  .forEach(path => removeSync(path))
+
+copySync("dist", paths.mods)
 
 notifier.notify({
   title: "7D2D Balance Changes",
